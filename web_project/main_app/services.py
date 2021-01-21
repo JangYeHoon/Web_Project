abc0ac2e20dea6e921bda8d11669a3b558453591
JangYeHoon <jkjk128jkjk@gmail.com>
2021-01-21 10:56:07 +0900

    출발지 목적지 중복 제거 및 정렬

--- web_project/main_app/services.py
+++ web_project/main_app/services.py
@@ -3,8 +3,8 @@ from air_app.models import Ticket
 
 class MainService:
     def searchPlace(self):
-        departure_place_list= Ticket.objects.all().values('departure_place')
-        arrival_place_list = Ticket.objects.all().values('arrival_place')
+        departure_place_list= Ticket.objects.all().values('departure_place').distinct().order_by('departure_place')
+        arrival_place_list = Ticket.objects.all().values('arrival_place').distinct().order_by('arrival_place')
         context ={"departure_place_list":departure_place_list,"arrival_place_list":arrival_place_list}
         print(departure_place_list)
         return context
